4cf568c40cbc601c1074b355ceb72e767abee154
Tan Le <tanld@vakaxa.com>
2018-11-26 01:56:41 -0700

    Express Simple REST

--- index.js
+++ index.js
@@ -4,17 +4,71 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const hostname = 'localhost';
 const port = 3000;
 
-// 1. Declare that we will use Express node module to constuct
+// 1. Declare that we will use Express node module to construct
 //    our web server.
 // We're saying that our application is going to use the express
 // node module. So once we say that, then Express provides a bunch
 // of methods that we can use to construct our web server
+
+// When ever we need to use a middle-ware, we say app.use()
 const app = express();
 app.use(morgan('dev'));
+app.use(bodyParser.json()); // allows us to parse the body of the request message
+
+
+
+// Using app.all(), with the first parameter is endpoint ('/dishes') and
+// the second parameter is the callback function.
+app.all('/dishes', (req, res, next) => {
+    // We are saying when a request comes in for all the requests, no matter which
+    // method is invoked GET, PUT, POST or DELETE for the '/dishes' REST API endpoint
+    // - When you call next() it will continue on to look for additional specifications
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+});
+
+app.get('/dishes', (req, res, next) => {
+    res.end('Will send all the dishes to you!');
+});
+
+app.post('/dishes', (req, res, next) => {
+    res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
+});
+
+app.put('/dishes', (req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /dishes');
+});
+
+app.delete('/dishes', (req, res, next) => {
+    res.end('Deleting all the dishes!');
+});
+// ---
+app.get('/dishes/:dishId', (req, res, next) => {
+    res.end('Will send details of the dishes: ' + req.params.dishId + ' to you!');
+});
+
+app.post('/dishes/:dishId', (req, res, next) => {
+    res.statusCode = 403;
+    res.end('POST operation not supported on /dishes/' + req.params.dishId);
+});
+
+app.put('/dishes/:dishId', (req, res, next) => {
+    res.write('Updating the dish: ' + req.params.dishId + '\n');
+    res.end('Will update the dish: ' + req.body.name +
+        ' with details: ' + req.body.description);
+});
+
+app.delete('/dishes/:dishId', (req, res, next) => {
+    res.end('Deleting dish: ' + req.params.dishId);
+});
+
 
 // We tell Express to serve up the static files from `__dirname` - the
 // root folder of the project
