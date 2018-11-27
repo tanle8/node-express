cd1c49252125a6a95ab767988399d4f75e9845a9
Tan Le <tanld@vakaxa.com>
2018-11-26 20:23:36 -0700

    Add router for /promoId

--- router/promoRouter.js
+++ router/promoRouter.js
@@ -29,5 +29,31 @@ promoRouter.route('/')
     res.end('Deleting all the promotions!');
 });
 
+// promoID
+dishRouter.route('/:promoId/')
+.all((req, res, next) => {
+    // We are saying when a request comes in for all the requests, no matter which
+    // method is invoked GET, PUT, POST or DELETE for the '/dishes/dishId' REST API endpoint
+    // - When you call next() it will continue on to look for additional specifications
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res, next) => {
+    res.end('Will send details of the dishes: ' + req.params.promoId + ' to you!');
+})
+.post((req, res, next) => {
+    res.statusCode = 403;
+    res.end('POST operation not supported on /promotions/' + req.params.promoId);
+})
+.put((req, res, next) => {
+    res.write('Updating the dish: ' + req.params.promoId + '\n');
+    res.end('Will update the dish: ' + req.body.name + ' with details: ' + req.params.promoId);
+})
+.delete((req, res, next) => {
+    res.end('Deleting dish: ' + req.params.promoId);
+});
+
+
 // Export this module
 module.exports = promoRouter;
